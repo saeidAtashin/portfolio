94dcbfa09448ae7177641ed7ca2702703cc604aa
saeidAtashin <105429979+saeidAtashin@users.noreply.github.com>
2025-11-17 17:38:01 +0330

    fixed images

--- src/data/WorkData.js
+++ src/data/WorkData.js
@@ -1,46 +1,45 @@
 export const Work = [
-    {
-        id:1,
-        name:"Ecommerce Shop",
-        description:"A Ecommerce shop using react and redux toolkit. Using fake REST API with the ability to edit and delete as Admin",
-        tags:["RESTAPI","toastify", "bootstrap"],
-        demo:"https://ecommerce-beryl-xi.vercel.app/product",
-        github:"https://github.com/saeidAtashin/ecommerce",
-        img:"https://www.freeiconspng.com/thumbs/responsive-icon/responsive-icon-9.png"
+  {
+    id: 1,
+    name: "Ecommerce Shop",
+    description:
+      "A Ecommerce shop using react and redux toolkit. Using fake REST API with the ability to edit and delete as Admin",
+    tags: ["RESTAPI", "toastify", "bootstrap"],
+    demo: "https://ecommerce-beryl-xi.vercel.app/product",
+    github: "https://github.com/saeidAtashin/ecommerce",
+    img: "https://www.freeiconspng.com/thumbs/responsive-icon/responsive-icon-9.png",
+  },
 
+  {
+    id: 2,
+    name: "Simplified Netflix",
+    description:
+      "A Movie listing website build in ReactJS. With react-redux axios.",
+    tags: ["react", "redux", "axios"],
+    demo: "https://netflix-used-api.vercel.app/",
+    github: "https://github.com/saeidAtashin/netflix",
+    img: "https://cdn.mos.cms.futurecdn.net/Yy247gYvzaMZXprhZHXy4E.jpg",
+  },
+  {
+    id: 3,
+    name: "Candy Crush Desktop",
+    description:
+      "It is Simplified Candy Crush Game for desktop in React JS, with html, css , javascript. It work for 3, 4 and 5 element in row and column of same color",
+    tags: ["react", "css", "axios", "dotenv"],
 
-    },
-    
-    {
-        id:2,
-        name:"Simplified Netflix",
-        description:"A Movie listing website build in ReactJS. With react-redux axios.",
-        tags:["react","redux", "axios"],
-        demo:"https://netflix-used-api.vercel.app/",
-        github:"https://github.com/saeidAtashin/netflix",
-        img:"https://cdn.mos.cms.futurecdn.net/Yy247gYvzaMZXprhZHXy4E.jpg"
-        
-    },
-    {
-        id:3,
-        name:"Candy Crush Desktop",
-        description:"It is Simplified Candy Crush Game for desktop in React JS, with html, css , javascript. It work for 3, 4 and 5 element in row and column of same color",
-        tags:["react","css","axios", "dotenv"],
-        
-        demo:"https://candy-crush-delta.vercel.app/",
-        github:"https://github.com/saeidAtashin/candyCrush",
-        img:"https://m.media-amazon.com/images/M/MV5BZWRiMjNlMjctOWZlMC00NTA0LWJlZTEtM2IxZDg0NTcyNWYxXkEyXkFqcGdeQXVyNTgyNTA4MjM@._V1_FMjpg_UX1000_.jpg"
-    }
-    ,
-    {
-        id:4,
-        name:"Three JS Airplane",
-        description:"This is a site built by TreeJS and it uses react three fiber, gsap and lamina. Working with camera and light and 3D objects is practiced in this project.",
-        tags:["gsap","lamina", "Three.js"],
-        
-        demo:"https://threejsairplane.vercel.app/",
-        github:"https://github.com/saeidAtashin/threejsairplane",
-        img:"https://cdn.pixabay.com/photo/2017/10/01/16/25/aircraft-2806035_1280.jpg"
-    }
+    demo: "https://candy-crush-delta.vercel.app/",
+    github: "https://github.com/saeidAtashin/candyCrush",
+    img: "https://www.cityam.com/wp-content/uploads/2023/09/CCS-Candies-Key-Art-4-3-Logo-e1695898049123.jpg?resize=742,557",
+  },
+  {
+    id: 4,
+    name: "Three JS Airplane",
+    description:
+      "This is a site built by TreeJS and it uses react three fiber, gsap and lamina. Working with camera and light and 3D objects is practiced in this project.",
+    tags: ["gsap", "lamina", "Three.js"],
 
-] 
+    demo: "https://threejsairplane.vercel.app/",
+    github: "https://github.com/saeidAtashin/threejsairplane",
+    img: "https://images.stockcake.com/public/f/b/6/fb6acf1b-fcaa-4152-b48b-6b0e2b0c5374_large/airplane-mid-flight-stockcake.jpg",
+  },
+];
